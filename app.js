0fa78db1e3a496713004513798b33728094e5e16
Abin Anto <106867862+abin98anto@users.noreply.github.com>
2022-11-08 11:42:00 +0530

    css pending - js complete

--- app.js
+++ app.js
@@ -1,6 +1,45 @@
-let gridSelect = document.getElementsByClassName("grid");
-let dps = 16;
-for(i=0;i<=dps;i++){
+let colour = 'black';
 
+function boardCreator(size) {
+    let board = document.querySelector('#container');
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div) => div.remove());
+    board.style.gridTemplateColumns=`repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows=`repeat(${size}, 1fr)`;
+    let amount = size * size;
+    for(let i=0; i< amount; i++){
+        let square = document.createElement('div'); 
+        square.addEventListener("mouseover", squareColour);
+        square.style.backgroundColor = 'white';
+        board.insertAdjacentElement('beforeend', square);
+    }
 }
 
+boardCreator(16);
+
+function sizeSelector(value) {
+    if(value < 2 || value > 100){
+        alert('cannot do that'); 
+    }else {
+        boardCreator(value); 
+    }
+    //console.log(value);
+}
+
+function squareColour() {
+    if(colour === "random"){
+        this.style.backgroundColor = `hsl(${Math.random()*360}, 100%, 50%)`;
+    }else {
+    this.style.backgroundColor = colour;
+    }
+}
+
+function changeColour(choice) {
+    colour = choice;
+}
+
+function reset(){
+    let board = document.querySelector('#container');
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div) => div.style.backgroundColor = "white");
+}
